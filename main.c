deb46d798cebcf54c0df91366bd2a2125b17007a
Dakror <mail@dakror.de>
2015-02-12 20:59:31 +0100

    and gate version

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
-/**
- @author Dakror
- **/
+/*
+ * Author: Dakror
+ */
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
 
 #define confidence_threshhold 10
+
+// -- displays -- //
+#define DSPA PINA0
+#define DSPB PINA1
+#define DSPC PINA2
+#define DSPD PINA3
+
+const uint8_t take_displays[] = { DSPC, DSPD };
+const uint8_t scene_displays[] = { DSPA };
 
 struct btn {
 	volatile uint8_t * sfr;
@@ -21,8 +30,7 @@ struct btn {
 	uint8_t released_confidence;
 };
 
-const char take_ports[] = { 'c', 'd' };
-const char scene_ports[] = { 'a' };
+// -- chars -- //
 
 char nums[] = {
 // - - - - - - HDCGBEFA
@@ -41,36 +49,33 @@ char nums[] = {
         '9', 0b01111011, //
         };
 
-// -- Methods -- //
-
-void displayIndex(uint8_t index, char port) {
-	if (port == 'a') PORTA = nums[index];
-	else if (port == 'c') PORTC = nums[index];
-	else if (port == 'd') PORTD = nums[index];
-}
-
 int lookup(char num) {
-	for (int i = 0; i < sizeof(nums); i += 2)
+	for (uint8_t i = 0; i < sizeof(nums); i += 2)
 		if (num == nums[i]) return i + 1;
 
 	return lookup('E'); // 'E' for error by default
 }
 
-void display(char num, char port) {
+// -- display -- //
+
+void displayIndex(uint8_t index, uint8_t port) {
+	PORTC = nums[index];
+	PORTA = 0b11111111; // all other pins of a are off
+}
+
+void display(char num, uint8_t port) {
 	displayIndex(lookup(num), port);
 }
 
-/**
- Display that an error has occured
- **/
 void err(void) {
-	display('E', 'a');
-	display('E', 'c');
-	display('E', 'd');
+	display('E', DSPA);
+	display('E', DSPB);
+	display('E', DSPC);
+	display('E', DSPD);
 }
 
-void displayNumber(uint8_t num, const char ports[], uint8_t portsc) {
-	int digits = floor(log10(num)) + 1;
+void displayNumber(uint8_t num, const uint8_t ports[], uint8_t portsc) {
+	uint8_t digits = floor(log10(num)) + 1;
 
 	if (digits > portsc) {
 		err();
@@ -79,11 +84,13 @@ void displayNumber(uint8_t num, const char ports[], uint8_t portsc) {
 
 	if (num == 0) digits = 1;
 
-	for (int i = 0; i < portsc; i++) {
+	for (uint8_t i = 0; i < portsc; i++) {
 		display((char) (((uint8_t) '0') + (num % 10)), ports[(portsc - 1) - i]);
 		num /= 10;
 	}
 }
+
+// -- button -- //
 
 void debounce(struct btn * b, void (*f)(void)) {
 	if (bit_is_clear(*(b->sfr), b->pin)) {
@@ -106,7 +113,7 @@ void debounce(struct btn * b, void (*f)(void)) {
 	}
 }
 
-// -- main -- //
+// -- main part -- //
 
 uint8_t scene = 1;
 uint8_t take = 1;
@@ -129,19 +136,20 @@ int main(void) {
 
 	DDRA = 0b11111111; // full a part on output
 	DDRC = 0b11111111; // full c part on output
-	DDRD = 0b11111111; // full d part on output
 
-	struct btn btn0 = { .sfr = &PINB, .pin = 0, .pressed = 0, .pressed_confidence = 0, .released_confidence = 0 };
+	/*struct btn btn0 = { .sfr = &PINB, .pin = 0, .pressed = 0, .pressed_confidence = 0, .released_confidence = 0 };
 	struct btn btn1 = { .sfr = &PINB, .pin = 1, .pressed = 0, .pressed_confidence = 0, .released_confidence = 0 };
+	 */
+
+	display('8', DSPC);
 
 	while (1) {
-		displayNumber(take, take_ports, 2);
-		displayNumber(scene, scene_ports, 1);
+		/*displayNumber(take, take_displays, 2);
+		displayNumber(scene, scene_displays, 1);
 
 		debounce(&btn0, btn0_press);
-		debounce(&btn1, btn1_press);
+		debounce(&btn1, btn1_press);*/
 	}
 
 	return 0;
 }
-
